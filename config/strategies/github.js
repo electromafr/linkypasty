1b8313f6b8c99e9fd6291e1b1d3d982b2f4b1785
electromafr <pablo.larvor@gmail.com>
2015-08-31 20:18:03 +0200

    little fix on authentification for gh and tw

--- config/strategies/github.js
+++ config/strategies/github.js
@@ -22,6 +22,11 @@ module.exports = function() {
 			providerData.accessToken = accessToken;
 			providerData.refreshToken = refreshToken;
 
+			// Create the user OAuth profile
+			var displayName = profile.displayName.trim();
+			var iSpace = displayName.indexOf(' '); // index of the whitespace following the firstName
+			var firstName =  iSpace !== -1 ? displayName.substring(0, iSpace) : displayName;
+			var lastName = iSpace !== -1 ? displayName.substring(iSpace + 1) : '';
 
 			var providerUserProfile = {
 				firstName: firstName,
