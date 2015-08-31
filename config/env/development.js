d45806dc0939a07d78105b0a252a548f8ef68d69
electromafr <pablo.larvor@gmail.com>
2015-08-31 16:10:59 +0200

    database

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: {
-		uri: 'mongodb://electromafr:ungorille@ds035603.mongolab.com:35603/linkypasty',
+		uri: 'mongodb://admin:1gorille@ds035603.mongolab.com:35603/linkypasty',
 		options: {
 			user: '',
 			pass: ''
